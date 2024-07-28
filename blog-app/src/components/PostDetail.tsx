b2b46301a70b6efaaad13c74cf9f5ce4fd1f3781
junhee <limjunhee4575@gmail.com>
2024-07-29 00:03:49 +0900

    24/07/29

---
+++ blog-app/src/components/PostDetail.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+export default function PostDetail() {
+  return (
+    <>
+      <div className="post__detail">
+        <div className="post__box">
+          <div className="post__title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </div>
+          <div className="post__profile-box">
+            <div className="post__profile" />
+            <div className="post__author-name">임준희</div>
+            <div className="post__data">2024.07.28 일요일</div>
+          </div>
+          <div className="post__utils-box">
+            <div className="post__delete">삭제</div>
+            <div className="post__edit">
+              <Link to={`/posts/edit/1`}>수정</Link>
+            </div>
+          </div>
+          <div className="post__text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ blog-app/src/components/Router.tsx
@@ -0,0 +1,30 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "pages/home";
+import PostList from "pages/posts";
+import PostDetail from "pages/posts/detail";
+import PostNew from "pages/posts/new";
+import PostEdit from "pages/posts/edit";
+import ProfilePage from "pages/profile";
+import LoginPage from "pages/login";
+import SignupPage from "pages/signup";
+
+export default function Router() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostList />} />
+        {/* 동적 라우트: :id 파라미터를 통해 특정 게시물의 상세 페이지로 이동 */}
+        <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/posts/new" element={<PostNew />} />
+        {/* 동적 라우트: :id 파라미터를 통해 특정 게시물의 수정 페이지로 이동 */}
+        <Route path="/posts/edit/:id" element={<PostEdit />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        {/* 정의되지 않은 경로로 접근 시 "/"로 리다이렉트 */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
+  );
+}
